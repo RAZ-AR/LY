85597314caa46800dc66762757d2e4ba2f1fa8b2
RAZ-AR <razmikovich@gmail.com>
2025-06-15 15:00:42 +0200

    commit

---
+++ LY/LY/LYTests/LYTests.swift
@@ -0,0 +1,17 @@
+//
+//  LYTests.swift
+//  LYTests
+//
+//  Created by Armen on 15/06/2025.
+//
+
+import Testing
+@testable import LY
+
+struct LYTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
